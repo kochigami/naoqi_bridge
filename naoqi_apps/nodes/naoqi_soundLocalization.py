9e25fd8571668517a201edac47fd933019e90a34
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2016-10-18 13:15:17 +0900

    modify naoqi_soundLocalization.py

--- naoqi_apps/nodes/naoqi_soundLocalization.py
+++ naoqi_apps/nodes/naoqi_soundLocalization.py
@@ -16,44 +16,102 @@
 #                                                                             
 # 
 import rospy
-from naoqi_bridge_msgs.msg import SoundLocated
 from naoqi_driver.naoqi_node import NaoqiNode
+from std_srvs.srv import(
+    EmptyResponse,
+    Empty)
+from naoqi_bridge_msgs.msg import SoundLocated
+from naoqi_bridge_msgs.srv import(
+    SetEnergyComputationResponse,
+    SetEnergyComputation,
+    SetSensitivityResponse,
+    SetSensitivity)
 
 class NaoqiSoundLocalization (NaoqiNode):
     def __init__(self):
         NaoqiNode.__init__(self, 'naoqi_soundLocalization')
         self.connectNaoQi()
         self.soundLocalization = SoundLocated()
+        self.pre_sound_localization = None
         self.soundLocalizationPub = rospy.Publisher("soundLocated", SoundLocated, queue_size=10)
+        self.subscribeSoundLocalizationSrv = rospy.Service("start_sound_localization", Empty, self.handleSubscribeSoundLocalization)
+        self.unsubscribeSoundLocalizationSrv = rospy.Service("stop_sound_localization", Empty, self.handleUnsubscribeSoundLocalization)
+        self.setEnergyComputationSrv = rospy.Service("set_energy_computation", SetEnergyComputation, self.handleSetEnergyComputation)
+        self.setSensitivitySrv = rospy.Service("set_sensitivity", SetSensitivity, self.handleSetSensitivity)
         rospy.loginfo("naoqi_soundLocalization is initialized")
        
     def connectNaoQi(self):
         rospy.loginfo("Connecting to NaoQi at %s:%d", self.pip, self.pport)
         self.memProxy = self.get_proxy("ALMemory")
-        
-        if self.memProxy is None:
+        self.soundLocalizationProxy = self.get_proxy("ALSoundLocalization")
+        if self.memProxy is None or self.soundLocalizationProxy is None:
             exit(1)
-       
+    
+    def handleSubscribeSoundLocalization(self, req):
+        try:
+            self.soundLocalizationProxy.subscribe("sound_localization")
+            return EmptyResponse()
+        except RuntimeError, e:
+            rospy.logerr("Exception caught:\n%s", e)
+            return None
+            
+    def handleUnsubscribeSoundLocalization(self, req):
+        try:
+            self.soundLocalizationProxy.unsubscribe("sound_localization")
+            return EmptyResponse()
+        except RuntimeError, e:
+            rospy.logerr("Exception caught:\n%s", e)
+            return None
+            
+    def handleSetEnergyComputation(self, req):
+        try:
+            res = SetEnergyComputationResponse()
+            self.soundLocalizationProxy.setParameter("EnergyComputation", req.status)
+            res.success = True
+            return res
+        except RuntimeError, e:
+            rospy.logerr("Exception caught:\n%s", e)
+            res.success = False
+            return res
+            
+    def handleSetSensitivity(self, req):
+        try:
+            res = SetSensitivityResponse()
+            self.soundLocalizationProxy.setParameter("Sensitivity", req.sensitivity)
+            res.success = True
+            return res
+        except RuntimeError, e:
+            rospy.logerr("Exception caught:\n%s", e)
+            res.success = False
+            return res
+
     def run(self):
         while self.is_looping():
             try:
                 r = rospy.Rate(5)
                 sound = self.memProxy.getData("ALSoundLocalization/SoundLocated")
-                if (len(sound)) > 0:
+                if (len(sound)) > 0 and sound != self.pre_sound_localization:
                     self.soundLocalization.header.stamp = rospy.get_rostime()
-                    self.soundLocalization.threshold.data = sound[1][0]
-                    for i in range(0, 2):
-                        self.soundLocalization.location.data.append(sound[1][i+1])
-                    for i in range(0, 6):
-                        self.soundLocalization.head_position.data.append(sound[2][i])
+                    self.soundLocalization.azimuth = sound[1][0]
+                    self.soundLocalization.elevation = sound[1][1]
+                    self.soundLocalization.confidence = sound[1][2]
+                    self.soundLocalization.energy = sound[1][3] 
+                    self.soundLocalization.head_position_frame_torso.linear.x = sound[2][0]
+                    self.soundLocalization.head_position_frame_torso.linear.y = sound[2][1]
+                    self.soundLocalization.head_position_frame_torso.linear.z = sound[2][2]
+                    self.soundLocalization.head_position_frame_torso.angular.x = sound[2][3]
+                    self.soundLocalization.head_position_frame_torso.angular.y = sound[2][4]
+                    self.soundLocalization.head_position_frame_torso.angular.z = sound[2][5]
+                    self.soundLocalization.head_position_frame_robot.linear.x = sound[3][0]
+                    self.soundLocalization.head_position_frame_robot.linear.y = sound[3][1]
+                    self.soundLocalization.head_position_frame_robot.linear.z = sound[3][2]
+                    self.soundLocalization.head_position_frame_robot.angular.x = sound[3][3]
+                    self.soundLocalization.head_position_frame_robot.angular.y = sound[3][4]
+                    self.soundLocalization.head_position_frame_robot.angular.z = sound[3][5]
+
                     self.soundLocalizationPub.publish(self.soundLocalization)
+                    self.pre_sound_localization = sound
                     
-                    for i in range(len(self.soundLocalization.location.data)):
-                        del self.soundLocalization.location.data[0]
-                    
-                    for i in range(len(self.soundLocalization.head_position.data)):
-                        del self.soundLocalization.head_position.data[0]
-                        
             except RuntimeError, e:
                 print "Error accessing ALMemory, exiting...\n"
                 print e

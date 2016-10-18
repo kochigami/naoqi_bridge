98e3f642a688ebf1afc93b840599d31bda90b555
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2016-10-18 20:10:00 +0900

    [wip] add naoqi_peoplePerception.py

--- naoqi_apps/nodes/naoqi_peoplePerception.py
+++ naoqi_apps/nodes/naoqi_peoplePerception.py
@@ -18,6 +18,37 @@
 import rospy
 from naoqi_bridge_msgs.msg import PeopleDetected
 from naoqi_driver.naoqi_node import NaoqiNode
+from std_srvs.srv import (
+    EmptyResponse,
+    Empty)
+from naoqi_bridge_msgs.srv import (
+    GetMaximumDetectionRangeResponse,
+    GetMaximumDetectionRange,
+    GetTimeBeforePersonDisappearsResponse,
+    GetTimeBeforePersonDisappears,
+    GetTimeBeforeVisiblePersonDisappearsResponse,
+    GetTimeBeforeVisiblePersonDisappears,
+    IsFaceDetectionEnabledResponse,
+    IsFaceDetectionEnabled,
+    IsFastModeEnabledResponse,
+    IsFastModeEnabled,
+    IsGraphicalDisplayEnabledResponse,
+    IsGraphicalDisplayEnabled,
+    IsMovementDetectionEnabledResponse,
+    IsMovementDetectionEnabled,
+    SetFastModeEnabledResponse,
+    SetFastModeEnabled,
+    SetGraphicalDisplayEnabledResponse,
+    SetGraphicalDisplayEnabled,
+    SetMaximumDetectionRangeResponse,
+    SetMaximumDetectionRange,
+    SetMovementDetectionEnabledResponse,
+    SetMovementDetectionEnabled,
+    SetTimeBeforePersonDisappearsResponse,
+    SetTimeBeforePersonDisappears,
+    SetTimeBeforeVisiblePersonDisappearsResponse,
+    SetTimeBeforeVisiblePersonDisappears
+    )
 
 class NaoqiPeoplePerception (NaoqiNode):
     def __init__(self):
@@ -26,15 +57,167 @@ class NaoqiPeoplePerception (NaoqiNode):
         self.peopleDetected = PeopleDetected()
         #self.prePeopleDetected = None
         self.peopleDetectedPub = rospy.Publisher("peopleDetected", PeopleDetected, queue_size=10)
+        self.getMaximumDetectionRangeSrv = rospy.Service("get_maximum_detection_range", GetMaximumDetectionRange, self.handleGetMaximumDetectionRange)
+        self.getTimeBeforePersonDisappearsSrv = rospy.Service("get_time_before_person_disappears", GetTimeBeforePersonDisappears , self.handleGetTimeBeforePersonDisappears)
+        self.getTimeBeforeVisiblePersonDisappearsSrv = rospy.Service("get_time_before_visible_person_disappears", GetTimeBeforeVisiblePersonDisappears , self.handleGetTimeBeforeVisiblePersonDisappears)
+        self.isFaceDetectionEnabledSrv = rospy.Service("is_face_detection_enabled", IsFaceDetectionEnabled, self.handleIsFaceDetectionEnabled)
+        self.isFastModeEnabledSrv = rospy.Service("get_fast_mode_enabled", IsFastModeEnabled, self.handleIsFastModeEnabled)
+        self.isGraphicalDisplayEnabledSrv = rospy.Service("is_graphical_display_enabled", IsGraphicalDisplayEnabled, self.handleIsGraphicalDisplayEnabled)
+        self.isMovementDetectionEnabledSrv = rospy.Service("get_movement_detection_enabled", IsMovementDetectionEnabled, self.handleIsMovementDetectionEnabled)
+        self.resetPopulationSrv = rospy.Service("reset_population", Empty, self.handleResetPopulation)
+        self.setFastModeEnabledSrv = rospy.Service("set_fast_mode_enabled", SetFastModeEnabled, self.handleSetFastModeEnabled)
+        self.setGraphicalDisplayEnabledSrv = rospy.Service("set_graphical_display_enabled", SetGraphicalDisplayEnabled, self.handleSetGraphicalDisplayEnabled)
+        self.setMaximumDetectionRangeSrv = rospy.Service("set_maximum_detection_range_enabled", SetMaximumDetectionRange, self.handleSetMaximumDetectionRange)
+        self.setMovementDetectionEnabledSrv = rospy.Service("set_move_detection_enabled", SetMovementDetectionEnabled, self.handleSetMovementDetectionEnabled)
+        self.setTimeBeforePersonDisappearsSrv = rospy.Service("set_time_before_person_disappears", SetTimeBeforePersonDisappears, self.handleSetTimeBeforePersonDisappears)
+        self.setTimeBeforeVisiblePersonDisappearsSrv = rospy.Service("set_time_before_visible_person_disappears", SetTimeBeforeVisiblePersonDisappears, self.handleSetTimeBeforeVisiblePersonDisappears)
         rospy.loginfo("naoqi_peoplePerception is initialized")
        
     def connectNaoQi(self):
         rospy.loginfo("Connecting to NaoQi at %s:%d", self.pip, self.pport)
         self.memProxy = self.get_proxy("ALMemory")
-        
-        if self.memProxy is None:
+        self.peoplePerceptionProxy = self.get_proxy("ALPeoplePerception")
+        if self.memProxy is None or self.peoplePerceptionProxy is None:
             exit(1)
-       
+            
+    def handleGetMaximumDetectionRange(self, req):
+        try:
+            res = GetMaximumDetectionRangeResponse()
+            res.ranges = self.peoplePerceptionProxy.getMaximumDetectionRange()  
+            return res
+        except RuntimeError, e:
+            rospy.logerr("Exception caught:\n%s", e)
+            return None
+
+    def handleGetTimeBeforePersonDisappears(self, req):
+        try:
+            res = GetTimeBeforePersonDisappearsResponse()
+            res.time_before_person_disappears = self.peoplePerceptionProxy.getTimeBeforePersonDisappears()  
+            return res
+        except RuntimeError, e:
+            rospy.logerr("Exception caught:\n%s", e)
+            return None
+
+    def handleGetTimeBeforeVisiblePersonDisappears(self, req):
+        try:
+            res = GetTimeBeforeVisiblePersonDisappearsResponse()
+            res.time_before_visible_person_disappears = self.peoplePerceptionProxy.getTimeBeforeVisiblePersonDisappears()  
+            return res
+        except RuntimeError, e:
+            rospy.logerr("Exception caught:\n%s", e)
+            return None
+    
+    def handleIsFaceDetectionEnabled(self, req):
+        try:
+            res = IsFaceDetectionEnabledResponse()
+            res.status = self.peoplePerceptionProxy.isFaceDetectionEnabled()  
+            return res
+        except RuntimeError, e:
+            rospy.logerr("Exception caught:\n%s", e)
+            return None
+
+    def handleIsFastModeEnabled(self, req):
+        try:
+            res = IsFastModeEnabledResponse()
+            res.status = self.peoplePerceptionProxy.isFastModeEnabled()  
+            return res
+        except RuntimeError, e:
+            rospy.logerr("Exception caught:\n%s", e)
+            return None
+
+    def handleIsGraphicalDisplayEnabled(self, req):
+        try:
+            res = IsGraphicalDisplayEnabledResponse()
+            res.status = self.peoplePerceptionProxy.isGraphicalDisplayEnabled()  
+            return res
+        except RuntimeError, e:
+            rospy.logerr("Exception caught:\n%s", e)
+            return None
+
+    def handleIsMovementDetectionEnabled(self, req):
+        try:
+            res = IsMovementDetectionEnabledResponse()
+            res.status = self.peoplePerceptionProxy.isMovementDetectionEnabled()  
+            return res
+        except RuntimeError, e:
+            rospy.logerr("Exception caught:\n%s", e)
+            return None
+
+    def handleResetPopulation(self, req):
+        try:
+            res = EmptyResponse()
+            self.peoplePerceptionProxy.resetPopulation()  
+            return res
+        except RuntimeError, e:
+            rospy.logerr("Exception caught:\n%s", e)
+            return None
+
+    def handleSetFastModeEnabled(self, req):
+        res = SetFastModeEnabledResponse()
+        res.success = False
+        try:
+            self.peoplePerceptionProxy.setFastModeEnabled(req.enable)  
+            res.success = True
+            return res
+        except RuntimeError, e:
+            rospy.logerr("Exception caught:\n%s", e)
+            return res
+
+    def handleSetGraphicalDisplayEnabled(self, req):
+        res = SetGraphicalDisplayEnabledResponse()
+        res.success = False
+        try:
+            self.peoplePerceptionProxy.setGraphicalDisplayEnabled(req.enable)  
+            res.success = True
+            return res
+        except RuntimeError, e:
+            rospy.logerr("Exception caught:\n%s", e)
+            return res
+
+    def handleSetMaximumDetectionRange(self, req):
+        res = SetMaximumDetectionRangeResponse()
+        res.success = False
+        try:
+            self.peoplePerceptionProxy.setMaximumDetectionRange(req.ranges)  
+            res.success = True
+            return res
+        except RuntimeError, e:
+            rospy.logerr("Exception caught:\n%s", e)
+            return res
+
+    def handleSetMovementDetectionEnabled(self, req):
+        res = SetMovementDetectionEnabledResponse()
+        res.success = False
+        try:
+            self.peoplePerceptionProxy.setMovementDetectionEnabled(req.enable)  
+            res.success = True
+            return res
+        except RuntimeError, e:
+            rospy.logerr("Exception caught:\n%s", e)
+            return res
+
+    def handleSetTimeBeforePersonDisappears(self, req):
+        res = SetTimeBeforePersonDisappearsResponse()
+        res.success = False
+        try:
+            self.peoplePerceptionProxy.setTimeBeforePersonDisappears(req.seconds)  
+            res.success = True
+            return res
+        except RuntimeError, e:
+            rospy.logerr("Exception caught:\n%s", e)
+            return res
+
+    def handleSetTimeBeforeVisiblePersonDisappears(self, req):
+        res = SetTimeBeforeVisiblePersonDisappearsResponse()
+        res.success = False
+        try:
+            self.peoplePerceptionProxy.setTimeBeforeVisiblePersonDisappears(req.seconds)  
+            res.success = True
+            return res
+        except RuntimeError, e:
+            rospy.logerr("Exception caught:\n%s", e)
+            return res
+
     def run(self):
         while self.is_looping():
             try:
